721a77aee1f7e96084337285311224fa3e9a3158
Bednarczyk <klby@gft.com>
2018-06-20 14:45:22 +0200

    refactor of security configurer

--- src/main/java/sa/common/config/ResourceServerSecurityConfiguration.java
+++ src/main/java/sa/common/config/ResourceServerSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package sa.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -9,11 +10,18 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerSecurityConfiguration extends ResourceServerConfigurerAdapter {
 
+    private final static String WRITE_SCOPE = "#oauth2.hasScope('write')";
+    private final static String READ_SCOPE = "#oauth2.hasScope('read')";
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
         http.requestMatchers()
                 .and().authorizeRequests()
-                .antMatchers("/users").access("#oauth2.hasScope('read')")
-                .anyRequest().access("#oauth2.hasScope('write')");
+                .antMatchers(HttpMethod.GET).access(READ_SCOPE)
+                .antMatchers(HttpMethod.GET).access(WRITE_SCOPE)
+                .antMatchers(HttpMethod.POST).access(WRITE_SCOPE)
+                .antMatchers(HttpMethod.PUT).access(WRITE_SCOPE)
+                .antMatchers(HttpMethod.DELETE).access(WRITE_SCOPE);
     }
 }
