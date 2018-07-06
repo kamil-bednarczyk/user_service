2598fbdcf5024ba84dd4901a43c14f99892fc672
Bednarczyk <klby@gft.com>
2018-07-06 11:11:24 +0200

    fix SpringApp loadContext() by adding qpid embedded server before test

--- src/test/java/sa/common/UserServiceApplicationTest.java
+++ src/test/java/sa/common/UserServiceApplicationTest.java
@@ -1,5 +1,9 @@
 package sa.common;
 
+import org.apache.qpid.server.Broker;
+import org.apache.qpid.server.BrokerOptions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +13,29 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class UserServiceApplicationTest {
 
+    private static Broker broker;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        embeddedQMQPBroker();
+    }
+
     @Test
     public void contextLoads() {
+    }
+
+    @AfterClass
+    public static void close() {
+        broker.shutdown();
+    }
+
+    private static void embeddedQMQPBroker() throws Exception {
+        broker = new Broker();
+        BrokerOptions brokerOptions = new BrokerOptions();
+        brokerOptions.setConfigProperty("qpid.amqp_port", "15673");
+        brokerOptions.setConfigProperty("qpid.broker.defaultPreferenceStoreAttributes", "{\"type\": \"Noop\"}");
+        brokerOptions.setConfigurationStoreType("Memory");
+        broker.startup(brokerOptions);
     }
 
 }
