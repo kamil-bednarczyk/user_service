0e3b6419d020450e3e1b1f2b648fed1d072d6230
Bednarczyk <klby@gft.com>
2018-07-09 16:17:58 +0200

    AccountActivationLinkIntegrationTests checking now if user is enabled after activation link event

--- src/test/java/sa/common/integration/AccountActivationLinkIntegrationTests.java
+++ src/test/java/sa/common/integration/AccountActivationLinkIntegrationTests.java
@@ -2,13 +2,16 @@ package sa.common.integration;
 
 import lombok.extern.log4j.Log4j2;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import sa.common.core.activationLink.command.ActivateAccountCommand;
 import sa.common.core.activationLink.command.CreateAccountActivationLinkCommand;
+import sa.common.core.user.CreateUserCommand;
 import sa.common.email.AccountActivationLink;
 import sa.common.email.ActivationLinkRepository;
 import sa.common.email.ActivationStatus;
+import sa.common.model.enums.Role;
 import sa.common.repository.UserRepository;
 
 import java.time.LocalDate;
@@ -27,41 +30,61 @@ public class AccountActivationLinkIntegrationTests extends BaseIntegrationTest {
     @Autowired
     private ActivationLinkRepository activationLinkRepository;
 
+    private CreateUserCommand createUserCommand;
+
+    @Before
+    public void setUp() {
+
+        createUserCommand = CreateUserCommand.builder()
+                .id("1234")
+                .username("username")
+                .password("password")
+                .email("test@localhost.com")
+                .isEnabled(false)
+                .avatar(new byte[100])
+                .role(Role.USER)
+                .build();
+
+        commandGateway.send(createUserCommand);
+    }
+
     @Test
-    public void sendAccountActivationLinkCommand() {
+    public void sendAccountActivationLinkCommand_ExpectAccountActivated() {
 
         String linkId = UUID.randomUUID().toString();
 
         AccountActivationLink expectedActivationLink = AccountActivationLink.builder()
                 .id(linkId)
-                .userId("eventId")
+                .userId(createUserCommand.getId())
                 .expirationDate(LocalDate.now().plusDays(1))
                 .status(ActivationStatus.NOT_ACTIVATED)
                 .build();
 
-        commandGateway.send(new CreateAccountActivationLinkCommand(linkId, "eventId"));
+        commandGateway.send(new CreateAccountActivationLinkCommand(linkId, createUserCommand.getId()));
         assertThat(activationLinkRepository.findById(linkId)).isEqualTo(Optional.of(expectedActivationLink));
 
         commandGateway.send(new ActivateAccountCommand(linkId, LocalDate.now()));
         assertThat(activationLinkRepository.findById(linkId).get().getStatus()).isEqualTo(ActivationStatus.ACTIVATED);
+        assertThat(userRepository.findById(expectedActivationLink.getUserId()).get().isEnabled()).isEqualTo(true);
     }
 
     @Test
-    public void sendAccountActivationLinkCommand2() {
+    public void sendAccountActivationLinkCommand_ExpectAccountActivationExpired() {
 
         String linkId = UUID.randomUUID().toString();
 
         AccountActivationLink expectedActivationLink = AccountActivationLink.builder()
                 .id(linkId)
-                .userId("eventId")
+                .userId(createUserCommand.getId())
                 .expirationDate(LocalDate.now().plusDays(1))
                 .status(ActivationStatus.NOT_ACTIVATED)
                 .build();
 
-        commandGateway.send(new CreateAccountActivationLinkCommand(linkId, "eventId"));
+        commandGateway.send(new CreateAccountActivationLinkCommand(linkId, createUserCommand.getId()));
         assertThat(activationLinkRepository.findById(linkId)).isEqualTo(Optional.of(expectedActivationLink));
 
         commandGateway.send(new ActivateAccountCommand(linkId, LocalDate.now().plusYears(3)));
         assertThat(activationLinkRepository.findById(linkId).get().getStatus()).isEqualTo(ActivationStatus.EXPIRED);
+        assertThat(userRepository.findById(expectedActivationLink.getUserId()).get().isEnabled()).isEqualTo(false);
     }
 }
